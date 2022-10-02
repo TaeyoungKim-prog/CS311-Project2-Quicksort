86b4203684ddbf721bca79ada1341ad85c65c3f2
Rice <teayong5@kaist.ac.kr>
2022-10-03 04:54:09 +0900

    print elementsa in list in terminal

--- quicksort.s
+++ quicksort.s
@@ -4,7 +4,7 @@
     array: .space 400
 
     out_string: .asciiz "\nEnter number of elements to be sorted: "
-    out_string_two: .asciiz "\nExited loop. "
+    out_string_two: .asciiz "\n"
     .text
 	
 .globl main
@@ -16,14 +16,15 @@ main:	# Start of code section
 
 	li $v0, 5
 	syscall		
-    add $t0, $zero, $v0 #t0 now stores number, n of elements A will have.
+    add $t0, $v0, $zero #t0 now stores number, n of elements A will have.
+    add $s0, $t0, $zero #store n for later use
     add $t1, $t0, $zero #t1 stores i = n.
     add $t2, $zero, $zero #initialize as the first position of array
     jal addNumbersToArray
 
-	li $v0, 4
-	la $a0, out_string_two
-	syscall		
+    add $t1, $s0, $zero #t1 stores i = n.
+    add $t2, $zero, $zero #initialize as the first position of array
+    jal printNumbersOfArray
 
 
 addNumbersToArray: # loop that iterates 5 times
@@ -36,6 +37,22 @@ addNumbersToArray: # loop that iterates 5 times
     
     addi	$t1, $t1, -1		# i = i - 1
     bne $t1, $zero, addNumbersToArray # if i is not zero continue loop
+    jr $ra  #when i = 0 exit loop and go back to main
+    
+printNumbersOfArray: # loop that iterates 5 times
+    lw   $t0, array($t2) #Loads from the array at each iteration.  
+    addi $t2, $t2, 4 #increment $t2 address by four for next index 
+    
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    li $v0, 4
+	la $a0, out_string_two
+	syscall		
+
+    addi	$t1, $t1, -1		# i = i - 1
+    bne $t1, $zero, printNumbersOfArray # if i is not zero continue loop
     jr $ra  #when i = 0 exit loop and go back to main
     
 
